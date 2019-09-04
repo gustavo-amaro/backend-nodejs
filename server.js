30e933919218751e81306bf77cf67bd05e724674
gustavo <gustavo.amaro97@gmail.com>
2019-09-04 15:18:44 -0300

    model and routes created

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir')
 
 //Iniciando o App
 const app = express();
@@ -9,5 +10,8 @@ app.use(cors());
 // Iniciando o DB
 const uri = "mongodb+srv://gustavo:123@cluster0-dppqo.mongodb.net/db?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true });
+requireDir('./src/models');
 
+// Rotas
+app.use('/api', require('./src/routes'));
 app.listen(3001);
